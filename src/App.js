64dae429f4828a945a39d99d10a82c6d24f6a3f2
trungkien0997sg <works.trungkien0997@gmail.com>
2024-05-13 04:10:01 +0700

    tao moi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/pages/HomePageTemplate/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <div className="bg-secondary py-3">Footer still working</div>
+    </div>
+  );
+}
